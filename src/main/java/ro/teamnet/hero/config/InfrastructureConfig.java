2a76aeb16578dca824882ba05e235ea0bf6873fb
Viorel Tacliciu <viorelt>
2013-12-10 16:29:06 +0200

    SpringConfiguration

---
+++ src/main/java/ro/teamnet/hero/config/InfrastructureConfig.java
@@ -0,0 +1,88 @@
+package ro.teamnet.hero.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * InfrastructureConfig.java
+ * <p/>
+ * Copyright (c) 2013 Teamnet. All Rights Reserved.
+ * <p/>
+ * This source file may not be copied, modified or redistributed,
+ * in whole or in part, in any form or for any reason, without the express
+ * written consent of Teamnet.
+ */
+@Configuration
+@EnableTransactionManagement
+public class InfrastructureConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    @Autowired
+    public DataSource dataSource(DatabasePopulator databasePopulator) {
+        EmbeddedDatabaseFactory factory = new EmbeddedDatabaseFactory();
+        factory.setDatabaseName("spring-social-showcase");
+        factory.setDatabaseType(EmbeddedDatabaseType.H2);
+        factory.setDatabasePopulator(databasePopulator);
+        return factory.getDatabase();
+    }
+
+    @Bean
+    public DatabasePopulator databasePopulator() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+//        populator.addScript(new ClassPathResource("Account.sql"));
+        populator.addScript(new ClassPathResource("data.sql"));
+        return populator;
+    }
+    protected
+    @Bean
+    JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter ret = new HibernateJpaVendorAdapter();
+        return completeVendor(ret);
+    }
+
+    private AbstractJpaVendorAdapter completeVendor(AbstractJpaVendorAdapter jpaVendorAdapter) {
+        jpaVendorAdapter.setDatabase(Database.H2);
+        jpaVendorAdapter.setGenerateDdl(true);
+        jpaVendorAdapter.setShowSql(true);
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    @Autowired
+    AbstractEntityManagerFactoryBean entityManagerFactory
+            (JpaVendorAdapter jpaVendorAdapter, DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean containerEntityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+        containerEntityManagerFactoryBean.setDataSource(dataSource);
+        containerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        containerEntityManagerFactoryBean.setPackagesToScan(domainPackages());
+        return containerEntityManagerFactoryBean;
+    }
+
+    private   String[] domainPackages(){
+        return new String[]{"ro.teamnet.hero.domain"};
+    }
+
+    @Bean
+    @Autowired
+    public org.springframework.transaction.PlatformTransactionManager transactionManagerCov(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
