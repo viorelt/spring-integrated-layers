2595aa2735477a4f9685a3b488ab23984b683d41
Viorel Tacliciu <viorelt>
2013-12-11 17:17:21 +0200

    pus Controller

--- src/main/java/ro/teamnet/hero/controller/AccountController.java
+++ src/main/java/ro/teamnet/hero/controller/AccountController.java
@@ -1,10 +1,12 @@
 package ro.teamnet.hero.controller;
 
+import flexjson.JSONDeserializer;
 import flexjson.JSONSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,9 +38,22 @@ public class AccountController {
         List<Account> accountList=accountService.findAll();
         JSONSerializer jsonSerializer=new JSONSerializer();
         String accountListJson=jsonSerializer
-                .exclude(".class")
+                .exclude("*.class")
                 .exclude("person")
                 .serialize(accountList);
         return new ResponseEntity<String>(accountListJson, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> createAccount(@RequestBody String json){
+        Account accountTransient=new JSONDeserializer<Account>()
+                .use(null, Account.class)
+                .deserialize(json);
+        Account accountDetached=accountService.createAccount(accountTransient);
+        return new ResponseEntity<String>(new JSONSerializer()
+                .exclude("*.class")
+                .exclude("person")
+                .serialize(accountDetached),HttpStatus.OK);
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,3 @@
-
-
-
 insert into PERSON(id_person,first_name,last_name) values (1,'first_name_test1','last_name_test1');
 insert into PERSON(id_person,first_name,last_name) values (2,'first_name_test2','last_name_test2');
 insert into ACCOUNT(id_account,user_name,password,email,id_person) values (1,'username_test1','secret1','test1@test.ro',1);
